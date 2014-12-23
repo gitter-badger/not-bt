105d0a3a0317be4122e4780ddda481818b58dc9c
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-23 10:25:52 -0500

    Adds happy path test

---
+++ spec/features/user_submits_report_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "Submit a report", %q{
+  As a user
+  I want to submit a report
+  So I can notify the city of an issue
+
+  Acceptance Criteria
+    [ ] I must enter a category and an address
+    [ ] If all fields are complete, I am told that my report has been saved
+    [ ] If a field is incomplete, I am given an error message and brought back to the input form.
+  } do
+
+    scenario 'user submits a report' do
+      report = FactoryGirl.build(:report)
+
+      visit new_report_path
+      fill_in "Category", with: report.category
+      fill_in "Address", with: report.address
+      click_on "Submit Report"
+      expect(page).to have_content("created successfully")
+    end
+
+end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,2 +1,6 @@
 FactoryGirl.define do
+  factory :report do
+    category "Streetlight"
+    location "33 Harrison Ave, Boston, MA 02111"
+  end
 end
