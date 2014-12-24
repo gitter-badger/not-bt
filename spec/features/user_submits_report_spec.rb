c1cbb8e3addf362f8398b23fb786e7fc80cb62a2
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-12-24 12:08:29 -0500

    Add login helper for user submits report

--- spec/features/user_submits_report_spec.rb
+++ spec/features/user_submits_report_spec.rb
@@ -19,12 +19,7 @@ feature 'Submit a report', %q(
   context 'authenticated user' do
     before :each do
       @user = FactoryGirl.create(:user)
-      visit new_user_session_path
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      within '.new_user' do
-        click_on 'Log in'
-      end
+      login_as(@user)
     end
 
     scenario 'user submits a report with valid attributes' do
